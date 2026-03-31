4db4738f68a0756c707530f158c3fbc387bbb827
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:51:14 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- lib/dogwatch/version.rb
+++
@@ -1,7 +0,0 @@
-# nodoc
-module DogWatch
-  VERSION = IO.read(File.expand_path('../../../VERSION', __FILE__)) rescue '0.0.1'
-  SUMMARY = 'A DSL to create DataDog Monitors'.freeze
-  DESCRIPTION = 'DogWatch provides a simple method for creating DataDog monitors in Ruby.'
-                .freeze
-end
